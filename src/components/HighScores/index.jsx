fa21dce76979ce4a32deb03f7445d53e1c785f3b
Hamza Eşsiz <webcatdev@gmail.com>
2022-08-21 23:34:58 +0300

    fixed color issue highscores

--- src/components/HighScores/index.jsx
+++ src/components/HighScores/index.jsx
@@ -1,10 +1,16 @@
 import classes from "./index.module.css";
 const HighScores = ({ scores }) => {
 const scorers = Object.keys(scores)
-    return (<>
-        {!scorers.length && <p>You have not played any game yet</p>}
-        <ul className={classes.scoreList}>{scorers.map(scorer => <li key={scorer}><span>{scorer}</span><span>{scores[scorer]}</span></li>)}</ul>
-    </>
+    return (
+        <ul className={classes.scoreList}>
+          {!scorers.length && <p>You have not played any game yet</p>}
+          {scorers.map((scorer) => (
+            <li key={scorer}>
+              <span>{scorer}</span>
+              <span>{scores[scorer]}</span>
+            </li>
+          ))}
+        </ul>
     );
 }
 
