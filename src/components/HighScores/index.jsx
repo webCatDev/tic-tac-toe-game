83871f83055948aa8cee076c955f034856739540
Hamza Eşsiz <webcatdev@gmail.com>
2022-08-21 19:49:34 +0300

    fixed no missing highscore text issue

--- src/components/HighScores/index.jsx
+++ src/components/HighScores/index.jsx
@@ -1,7 +1,10 @@
 import classes from "./index.module.css";
 const HighScores = ({ scores }) => {
-    return (
-        <ul className={classes.scoreList}>{Object.keys(scores).map(scorer => <li key={scorer}><span>{scorer}</span><span>{scores[scorer]}</span></li>)}</ul>
+const scorers = Object.keys(scores)
+    return (<>
+        {!scorers.length && <p>You have not played any game yet</p>}
+        <ul className={classes.scoreList}>{scorers.map(scorer => <li key={scorer}><span>{scorer}</span><span>{scores[scorer]}</span></li>)}</ul>
+    </>
     );
 }
 
