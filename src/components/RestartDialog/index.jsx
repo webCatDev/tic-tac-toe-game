790fdd63663303a5f311534fe07c3cc0332b9362
Hamza Eşsiz <webcatdev@gmail.com>
2022-08-30 16:53:59 +0300

    fixed restart dialog animation

--- src/components/RestartDialog/index.jsx
+++ src/components/RestartDialog/index.jsx
@@ -1,43 +1,46 @@
 import IconForDraw from "../Icons/IconForDraw";
 import IconForO from "../Icons/IconForO";
 import IconForX from "../Icons/IconForX";
-import classes from './index.module.css';
+import classes from "./index.module.css";
 import Languages from "../../Languages";
 
-const RestartDialog = ({  dispatcher, gameState }) => {
-    
-    const handleClick = () => {
-        dispatcher({type: 'handleRestart'})
-    }
+const RestartDialog = ({ dispatcher, gameState }) => {
+  const handleClick = () => {
+    dispatcher({ type: "handleRestart" });
+  };
 
-    return (
-      <div className={`${classes.restartDialog} ${gameState.winner === "X" || gameState.winner === "O" && classes.winner}`}>
-        <h1>
-          {gameState.winner === "X" || gameState.winner === "O"
-            ? Languages[gameState.lang].restartDialog.winnerText[0]
-            : Languages[gameState.lang].restartDialog.winnerText[1]}
-        </h1>
-        {gameState.winner === "X" ? (
-          <IconForX />
-        ) : gameState.winner === "O" ? (
-          <IconForO />
-        ) : (
-          <IconForDraw />
-        )}
+  return (
+    <div
+      className={`${classes.restartDialog} ${
+        (gameState.winner === "X" || gameState.winner === "O") && classes.winner
+      }`}
+    >
+      <h1>
+        {gameState.winner === "X" || gameState.winner === "O"
+          ? Languages[gameState.lang].restartDialog.winnerText[0]
+          : Languages[gameState.lang].restartDialog.winnerText[1]}
+      </h1>
+      {gameState.winner === "X" ? (
+        <IconForX />
+      ) : gameState.winner === "O" ? (
+        <IconForO />
+      ) : (
+        <IconForDraw />
+      )}
 
-        <button
-          className={classes.restartButton}
-          aria-label={Languages[gameState.lang].restartDialog.restartGameText}
-          onClick={handleClick}
-        >
-          {Languages[gameState.lang].restartDialog.restartGameText}
-        </button>
+      <button
+        className={classes.restartButton}
+        aria-label={Languages[gameState.lang].restartDialog.restartGameText}
+        onClick={handleClick}
+      >
+        {Languages[gameState.lang].restartDialog.restartGameText}
+      </button>
 
-        <button onClick={() => dispatcher({ type: "handleMainMenu" })}>
-          {Languages[gameState.lang].restartDialog.mainMenuText}
-        </button>
-      </div>
-    );
-}
+      <button onClick={() => dispatcher({ type: "handleMainMenu" })}>
+        {Languages[gameState.lang].restartDialog.mainMenuText}
+      </button>
+    </div>
+  );
+};
 
 export default RestartDialog;
